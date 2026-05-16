573525323ab700a4c132af02be7fc77d2e53ec38
DanielOsgu <danielosgu@icloud.com>
2026-05-16 16:10:05 -0400

    APIKEY reference fix

--- api/mapstyle.js
+++ api/mapstyle.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const key = process.env.STADIA_API_KEY;
+  const key = process.env.STADIAMAPS_API_KEY;
 
   res.status(200).json({
     url: `https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png?api_key=${key}`
