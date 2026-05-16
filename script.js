5c73b4e108c95423dabfa2ae3606dada3aded42a
DanielOsgu <danielosgu@icloud.com>
2026-05-16 11:57:12 -0400

    v3

--- script.js
+++ script.js
@@ -17,33 +17,45 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
 
-const beachIcon = L.divIcon({
-  html: "🌊",
-  className: "beach-icon",
-  iconSize: [35, 35],
-  iconAnchor: [17, 17]
-});
-
-L.marker(BARLOVENTO, { icon: beachIcon })
-  .addTo(map)
-  .bindPopup("<b>Barlovento Beach</b><br>Dorado Beach")
-  .openPopup();
-
 const spots = [
-  { name: "Barlovento / GoodWinds", coords: [18.4769, -66.2796], emoji: "🌊" },
-  { name: "Dorado Beach", coords: [18.4729, -66.2781], emoji: "🏖️" },
-  { name: "Playa Sardinera", coords: [18.4715, -66.2725], emoji: "📍" }
+  { name: "Barlovento / GoodWinds", coords: [18.4769, -66.2796] },
+  { name: "Dorado Beach", coords: [18.4729, -66.2781] },
+  { name: "Playa Sardinera", coords: [18.4715, -66.2725] }
 ];
 
 spots.forEach(spot => {
-  const icon = L.divIcon({
-    html: spot.emoji,
-    className: "beach-icon",
-    iconSize: [35, 35],
-    iconAnchor: [17, 17]
-  });
-
-  L.marker(spot.coords, { icon })
+  L.marker(spot.coords)
     .addTo(map)
-    .bindPopup(`<b>${spot.name}</b>`);
+    .bindPopup(`<b>${spot.name}</b><br>${spot.coords[0]}, ${spot.coords[1]}`);
+});
+
+// click map to get coordinates
+map.on("click", function(e) {
+  alert("Lat: " + e.latlng.lat + "\nLng: " + e.latlng.lng);
+});
+
+// location button
+const locateControl = L.control({ position: "topright" });
+
+locateControl.onAdd = function() {
+  const button = L.DomUtil.create("button", "locate-btn");
+  button.innerHTML = "📍";
+  button.title = "Find my location";
+
+  L.DomEvent.disableClickPropagation(button);
+
+  button.onclick = function() {
+    map.locate({ setView: true, maxZoom: 16 });
+  };
+
+  return button;
+};
+
+locateControl.addTo(map);
+
+map.on("locationfound", function(e) {
+  L.marker(e.latlng)
+    .addTo(map)
+    .bindPopup("You are here")
+    .openPopup();
 });
